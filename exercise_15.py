a6eced0ff533785129b7945b6a704626ede228bb
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 22:46:54 +0800

    description

---
+++ exercise_15.py
@@ -0,0 +1,7 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 15: 
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
